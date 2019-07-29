aa171326f373e5b4932916b04d359a51b598aa59
Phonekham <phone.madridista@gmail.com>
2019-07-29 17:10:48 +0700

    14. Making the API Better Filtering

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,7 +2,22 @@ const Tour = require("../models/tourModel");
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    // Build Query
+    const queryObj = { ...req.query };
+    const excludedFields = ["page", "sort", "limit", "fields"];
+    excludedFields.forEach(el => delete queryObj[el]);
+    console.log(req.query, queryObj);
+    const query = Tour.find(queryObj);
+    // Execute Query
+    const tours = await query;
+
+    // const tours = await Tour.find()
+    //   .where("duration")
+    //   .equals(5)
+    //   .where("difficulty")
+    //   .equals("easy");
+
+    // Send Response
     res.status(200).json({
       requestedAt: req.requestTime,
       status: "success",
