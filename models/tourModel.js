2672d2c32e73fa296631d833b6c167891ea53fc7
Phonekham <phone.madridista@gmail.com>
2019-07-29 15:14:11 +0700

    12. Modelling the Tours

--- models/tourModel.js
+++ models/tourModel.js
@@ -4,13 +4,53 @@ const tourSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, "A tour must have name"],
-    unique: true
+    unique: true,
+    trim: true
   },
-  rating: Number,
+  durations: {
+    type: Number,
+    required: [true, "A tour must have a duration"]
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, "A tour must have a group size"]
+  },
+  difficulty: {
+    type: String,
+    required: [true, "A tour must have difficulty"]
+  },
+  ratingsAverage: {
+    type: Number,
+    default: 4.5
+  },
+  ratingQuantity: {
+    type: Number,
+    default: 0
+  },
   price: {
     type: Number,
     required: [true, "A tour must have price"]
-  }
+  },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, "A tour must have have a description"]
+  },
+  description: {
+    type: String,
+    trim: true
+  },
+  imageCover: {
+    type: String,
+    required: [true, "A tour must have  a cover image"]
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  },
+  startDate: [Date]
 });
 const Tour = mongoose.model("Tour", tourSchema);
 
