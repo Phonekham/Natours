1249e41a264d2d4468dfe4ab1efa0d2929672ad6
Phonekham <phone.madridista@gmail.com>
2019-07-30 15:37:07 +0700

    19. Making the API Better Aliasing
    Middleware Top 5 Cheap

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -2,10 +2,9 @@ const express = require("express");
 const tourController = require("../controllers/tourController");
 const router = express.Router();
 
-// create a checkbody middleware
-// Check if body contains the name and price property
-// If not, send back 400(bad request)
-// Add it to the post handler stack
+router
+  .route("/top-5-cheap")
+  .get(tourController.aliasTopTours, tourController.getAllTours);
 
 router
   .route("/")
