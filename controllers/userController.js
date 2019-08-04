4c00ff00f64679ea04829c547337f77b1c6a21cf
Phonekham <phone.madridista@gmail.com>
2019-08-04 15:08:11 +0700

    7. Logging in Users

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,17 @@
-exports.getAllUsers = (req, res) => {
+const User = require("../models/userModel");
+const catchAsync = require("../utils/catchAsync");
+
+exports.getAllUsers = catchAsync(async (req, res, next) => {
+  const users = await User.find();
+  // Send Response
   res.status(500).json({
-    status: "error",
-    message: "This route is not defined"
+    status: "success",
+    results: users.length,
+    data: {
+      users
+    }
   });
-};
+});
 exports.getUser = (req, res) => {
   res.status(500).json({
     status: "error",
