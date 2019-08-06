85a894e8c8ecd8025898303484d2e53530717574
Phonekham <phone.madridista@gmail.com>
2019-08-06 17:16:55 +0700

    11. Authorization User Roles and Permissions

--- models/userModel.js
+++ models/userModel.js
@@ -15,6 +15,11 @@ const userSchema = new mongoose.Schema({
     validate: [validator.isEmail, "Plase provide a valid email"]
   },
   photo: String,
+  role: {
+    type: String,
+    enum: ["user", "lead-guide", "admin"],
+    default: "user"
+  },
   password: {
     type: String,
     required: [true, "Plase provide password"],
