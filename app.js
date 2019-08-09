b79a4764a8002525e792a4fd853962ab804cb565
Phonekham <phone.madridista@gmail.com>
2019-08-09 15:24:18 +0700

    22. Data Sanitization

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const morgan = require("morgan");
 const rateLimit = require("express-rate-limit");
 const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
 
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
@@ -31,6 +33,12 @@ app.use("/api", limiter);
 app.use(
   express.json({ limit: "10kb" /*limit amount of data that come in to body*/ })
 );
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+// Data sanitization against XSS
+app.use(xss()); //prevent html mulicious
+
 // Serving static file
 app.use(express.static(`${__dirname}/public`));
 
