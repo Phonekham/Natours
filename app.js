be3489e22d2d497584caf0393b8eedbd0173649a
Phonekham <phone.madridista@gmail.com>
2019-08-09 15:39:48 +0700

    23. Preventing Parameter Pollution

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const rateLimit = require("express-rate-limit");
 const helmet = require("helmet");
 const mongoSanitize = require("express-mongo-sanitize");
 const xss = require("xss-clean");
+const hpp = require("hpp");
 
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
@@ -38,6 +39,19 @@ app.use(
 app.use(mongoSanitize());
 // Data sanitization against XSS
 app.use(xss()); //prevent html mulicious
+// Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      "duration",
+      "ratingsQuantity",
+      "ratingsAverage",
+      "maxGroupSize",
+      "difficulty",
+      "price"
+    ]
+  })
+);
 
 // Serving static file
 app.use(express.static(`${__dirname}/public`));
