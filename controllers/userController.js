b85008022f9d5cd98ee3531c26e5621e70c281a1
Phonekham <phone.madridista@gmail.com>
2019-08-13 14:12:42 +0700

    16. Factory Functions Update and Create

--- controllers/userController.js
+++ controllers/userController.js
@@ -64,16 +64,7 @@ exports.getUser = (req, res) => {
     message: "This route is not defined"
   });
 };
-exports.createUser = (req, res) => {
-  res.status(500).json({
-    status: "error",
-    message: "This route is not defined"
-  });
-};
-exports.updateUser = (req, res) => {
-  res.status(500).json({
-    status: "error",
-    message: "This route is not defined"
-  });
-};
+exports.createUser = factory.createOne(User);
+// Do not update password with this
+exports.updateUser = factory.updateOne(User);
 exports.deleteUser = factory.deleteOne(User);
