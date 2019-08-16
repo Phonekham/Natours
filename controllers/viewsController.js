db9b648808ceb9fbf910a0af106bb9143960f6d5
Phonekham <phone.madridista@gmail.com>
2019-08-16 16:48:57 +0700

    build the tour page

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -8,8 +8,14 @@ exports.getOverview = catchAsync(async (req, res) => {
     tours
   });
 });
-exports.getTour = (req, res) => {
-  res.status(200).render("tour", {
-    title: "all tours"
+exports.getTour = catchAsync(async (req, res) => {
+  // Get the data for the requested tour (including reviews and guides)
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: "reviews",
+    fields: "review rating user"
   });
-};
+  res.status(200).render("tour", {
+    title: "all tours",
+    tour
+  });
+});
