f61fdee68f21382d0d1c8e0c10bdcd59a3821f88
Phonekham <phone.madridista@gmail.com>
2019-08-17 15:03:20 +0700

    15. Building the Login Screen

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -8,6 +8,7 @@ exports.getOverview = catchAsync(async (req, res) => {
     tours
   });
 });
+
 exports.getTour = catchAsync(async (req, res) => {
   // Get the data for the requested tour (including reviews and guides)
   const tour = await Tour.findOne({ slug: req.params.slug }).populate({
@@ -19,3 +20,9 @@ exports.getTour = catchAsync(async (req, res) => {
     tour
   });
 });
+
+exports.getLoginForm = (req, res) => {
+  res.status(200).render("login", {
+    title: "Log into your account"
+  });
+};
