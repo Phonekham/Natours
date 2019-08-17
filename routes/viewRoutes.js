023250cfe171d21df3d77c2f9527742ad8ad34d0
Phonekham <phone.madridista@gmail.com>
2019-08-17 16:35:02 +0700

    Logging in Users with Our API - Part 1-2

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,6 +1,10 @@
 const express = require("express");
-const router = express.Router();
 const viewsController = require("../controllers/viewsController");
+const authController = require("../controllers/authController");
+
+const router = express.Router();
+
+router.use(authController.isLoggedIn);
 
 router.get("/", viewsController.getOverview);
 router.get("/tour/:slug", viewsController.getTour);
