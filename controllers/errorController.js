5d45da954fa7ccca9edf8c6a548c2b6a80052416
Phonekham <phone.madridista@gmail.com>
2019-08-18 15:05:00 +0700

    20. Rendering Error Pages

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -24,30 +24,60 @@ const handleJWTError = () =>
 const handleJWTExpiredError = () =>
   new AppError("Your token has expired! Please login again", 401);
 
-const sendErrorDev = (err, res) => {
-  res.status(err.statusCode).json({
-    status: err.status,
-    error: err,
-    message: err.message,
-    stack: err.stack
+const sendErrorDev = (err, req, res) => {
+  //A) API
+  if (req.originalUrl.startsWith("/api")) {
+    return res.status(err.statusCode).json({
+      status: err.status,
+      error: err,
+      message: err.message,
+      stack: err.stack
+    });
+  }
+  //B) Rendered website view
+  console.log("ERROR", err);
+  return res.status(err.statusCode).render("error", {
+    title: "Something went wrong",
+    msg: err.message
   });
 };
 
-const sendErrorProd = (err, res) => {
-  // Operational, trusted error: send message to client
-  if (err.isOperational) {
-    res.status(err.statusCode).json({
-      status: err.status,
-      message: err.message
-    });
-    // Programming or other unknown error :don't leak error details
-  } else {
+const sendErrorProd = (err, req, res) => {
+  //A API
+  if (req.originalUrl.startsWith("/api")) {
+    //A Operational, trusted error: send message to client
+    if (err.isOperational) {
+      return res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message
+      });
+      //B) Programming or other unknown error :don't leak error details
+    }
+    // 1) Log Error
     console.log("ERROR", err);
-    res.status(500).json({
+    // 2) Send generic message
+    return res.status(500).json({
       status: "error",
       message: "Something went wrong"
     });
   }
+  //B) Rendered website view
+  //A Operational, trusted error: send message to client
+  if (err.isOperational) {
+    console.log(err.message);
+    return res.status(err.statusCode).render("error", {
+      title: "Something went wrong",
+      msg: err.message
+    });
+  }
+  // Programming or other unknown error :don't leak error details
+  // 1) log error
+  console.log("ERROR", err);
+  // 2) send generic message
+  return res.status(err.statusCode).render("error", {
+    title: "Something went wrong",
+    msg: "Please Try Again later"
+  });
 };
 
 module.exports = (err, req, res, next) => {
@@ -56,15 +86,16 @@ module.exports = (err, req, res, next) => {
   err.status = err.status || "error";
 
   if (process.env.NODE_ENV === "development") {
-    sendErrorDev(err, res);
+    sendErrorDev(err, req, res);
   } else if (process.env.NODE_ENV === "production") {
     let error = { ...err };
+    error.message = err.message;
     if (error.name === "CastError") error = handleCastErrorDB(error);
     if (error.code === 11000) error = handleDuplicateFieldsDB(error);
     if (error.name === "ValidationError")
       error = handleValidationErrorDB(error);
     if (error.name === "JsonWebTokenError") error = handleJWTError();
     if (error.name === "TokenExpiredError") error = handleJWTExpiredError();
-    sendErrorProd(error, res);
+    sendErrorProd(error, req, res);
   }
 };
--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,5 +1,6 @@
 const Tour = require("../models/tourModel");
 const catchAsync = require("../utils/catchAsync");
+const AppError = require("../utils/AppError");
 
 exports.getOverview = catchAsync(async (req, res) => {
   const tours = await Tour.find();
@@ -9,12 +10,17 @@ exports.getOverview = catchAsync(async (req, res) => {
   });
 });
 
-exports.getTour = catchAsync(async (req, res) => {
+exports.getTour = catchAsync(async (req, res, next) => {
   // Get the data for the requested tour (including reviews and guides)
   const tour = await Tour.findOne({ slug: req.params.slug }).populate({
     path: "reviews",
     fields: "review rating user"
   });
+
+  if (!tour) {
+    return next(new AppError("Tere is no tour with that name", 404));
+  }
+
   res.status(200).render("tour", {
     title: `${tour.name} tour`,
     tour
